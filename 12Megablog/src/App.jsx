fa2d0443512c149f081db27d8d63303a9970fce6
tan22-max <184149842+tan22-max@users.noreply.github.com>
2026-06-04 21:49:23 +0530

    1

--- 12Megablog/src/App.jsx
+++ 12Megablog/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+  
   return (
     <>
      <h1>blog site with appwrite</h1>
